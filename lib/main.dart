b694f83eceac723c51cb778add0ab75681dff3d0
hamzapromax <mehamza896@gmail.com>
2022-08-01 14:46:12 +0500

    Day 3 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:catalog_app/home_page.dart';
+// ignore_for_file: prefer_const_constructors
+
+import 'package:catalog_app/pages/home_page.dart';
+import 'package:catalog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // ignore: prefer_const_constructors
   runApp(MyApp());
 }
 
@@ -12,8 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        //home: HomePage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+        ),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.red,
+        ),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
